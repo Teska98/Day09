12f8c8e1c9bad71540daedb12848cbd61f8f0e1c
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-16 23:36:57 +0100

    Create index.js

---
+++ ex11/src/index.js
@@ -0,0 +1,25 @@
+function switchCase(letter){
+   // Write code below this line
+  case a:
+   var animals = "antelope";
+   break;
+  case b:
+   animals = "bird";
+   break;
+  case c:
+   animals = "cat";
+   break;
+  default:
+   animals = "stuff";
+   break
+  // Write code above this line
+  return animals;
+}
+
+
+console.log(switchCase("a"));
+console.log(switchCase("b"));
+console.log(switchCase("c"));
+console.log(switchCase("d"));
+console.log(switchCase(4));
+module.exports = switchCase;
