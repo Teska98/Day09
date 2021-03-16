f403e6301eade9c4d48bdfd2349315569476a08d
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-16 18:04:26 +0100

    Create index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+   function logicalOrOperator(num){
+     if(num => 20 || num <= 30){
+     return "In";
+     }
+     return "Out";
+   }
+logicalOrOperator(0);
+logicalOrOperator(9);
+logicalOrOperator(20);
+logicalOrOperator(23);
+logicalOrOperator(30);
+logicalOrOperator(31);
+logicalOrOperator(105);
+// Only change code above this line
+module.exports = logicalOrOperator;
