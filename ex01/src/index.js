1bf39513b9c70472dadc70c6286dc2028d2a07f3
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-16 16:47:45 +0100

    Create index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+   function myTrueBoolean(){
+     return boolean (10 > 9);
+   }
+console.log(myTrueBoolean());
+// Only change code above this line
+module.exports = myTrueBoolean;
