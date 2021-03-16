f5f5ba59ae358ecbc182f05d5033226375f54b2c
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-17 00:42:00 +0100

    Create index.js

---
+++ ex15/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+   function myDoWhile(){
+     var myNumbers = 0;
+     do {
+     console.log(myNumbers);
+       myNumbers++;
+     } while (myNumbers < 10);
+   }
+// Only change code above this line
+module.exports = myDoWhile;
