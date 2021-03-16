78ade24c2ff4729752d3fa69e74f16741bd46301
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-16 17:18:30 +0100

    Create index.js

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+   function compareDifferentValues(m, n){
+     if(m, n === 8){
+     return "Equal";
+     }
+     return "Not equal";
+   }
+compareDifferentValues(8, "8");
+compareDifferentValues("8", 8);
+compareDifferentValues(˙8˙, 8);
+compareDifferentValues("8", "8");
+compareDifferentValues(8, 8);
+// Only change code above this line
+module.exports = compareDifferentValues;
